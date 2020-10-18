bb8bade7b641030edf38b4a71418981e65f40b54
khanh9a1999 <70497384+khanh9a1999@users.noreply.github.com>
2020-10-18 21:20:35 +0700

    update

--- src/app/service/base-component.ts
+++ src/app/service/base-component.ts
@@ -1,7 +1,7 @@
 import { of as observableOf, fromEvent, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { FileUpload } from 'primeng/fileupload';
-import { ApiService } from '../lib/api.service';
+import { ApiService } from '../service/api-service';
 import { ActivatedRoute } from '@angular/router';
 import { Injector } from '@angular/core';
 export class BaseComponent {
